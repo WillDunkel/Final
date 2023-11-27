2774ef3c08f855d603c366fafa933d37a79ee6bd
William Dunkel <wndunkel@gmail.com>
2023-11-27 09:46:58 -0600

    Initial Commit

---
+++ Final/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Final
+//
+//  Created by William Dunkel on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by William Dunkel on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
