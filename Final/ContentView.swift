a7bc80eaf57e14f14a31d90623397a772885a4de
William Dunkel <wndunkel@gmail.com>
2023-11-30 09:27:32 -0600

    Made login text field and button , need to make a real password check

--- Final/ContentView.swift
+++ Final/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var loginId = Login()
+    @State var name: String = ""
+    @State var password: String = ""
+    @State var showPassword: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+       
+        NavigationView{
+            VStack {
+                Text("Login")
+                    .bold()
+                    .font(Font.custom("", size: 70))
+                
+                
+                Spacer()
+                Text("Please Enter Your Key Here")
+                    .bold()
+                VStack{
+                    CustomTextField(placeholder: "Key", variable: $loginId.Fname)
+                    
+                    
+                    
+                }
+                
+                    NavigationLink("Login", destination: SwiftUIView())
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+            }
+           
         }
-        .padding()
+       
     }
 }
 
@@ -23,4 +48,21 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct CustomTextField: View { let placeholder : String
+    let variable : Binding<String>
+    var body: some View {
+        TextField(placeholder, text: variable)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization (.none)
+    }
+}
+
+
+public struct Login {
+    var Id = ""
+    var Fname = ""
+    var Lname = ""
+    var twelve = "12"
+    
 }
---
+++ Final/SwiftUIView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  Final
+//
+//  Created by William Dunkel on 11/29/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack{
+            Text("Welcome to Fortnite Battle Royale")
+        }
+        
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
